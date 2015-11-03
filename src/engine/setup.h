d1fa5542313925a08ea4cafcf77f8a6c6f783208
EternalCode <dilshan.gunasekera@mail.utoronto.ca>
2015-11-03 17:14:29 -0500

    Additions to command setup
    Added code  which resets the graphical slate in preparation for cut
    scene. Also stealthily added a status checker to check if previous
    command executed, executed successfully.

---
+++ src/engine/setup.h
@@ -0,0 +1,16 @@
+/* Functions and mem addr for cutscene preparation */
+
+void script_env_2_enable(void);	
+void help_system_disable_sp198(void);
+void pal_set_something(u8, u8, u8, u8, u8);
+void overworld_free_bgmaps(void);
+void lcd_io_set(u8, u8);
+void vblank_hander_set(void (*callback)(u8));
+void vblank_hander_callback(void);
+void gpu_tile_bg_drop_all_sets(u8);
+void bg_positions_reset(void);
+void reset_and_init_alot(void);
+void bgid_nullify_tilemap(u8);
+u32 *maphook = (u32*)0x3005020;
+void mapldr_continue_scripts_restart_music(void);
+void help_system_enable_unless_flashback__sp199(void);
