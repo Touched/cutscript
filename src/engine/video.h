a8e8374ba2e3cfb6c2bc0644aaca3c66234a12f5
Touched <jtouched@gmail.com>
2015-11-04 15:38:36 +0200

    Add fade control structure

--- src/engine/video.h
+++ src/engine/video.h
@@ -3,6 +3,19 @@
 
 #include "types.h"
 
+struct fade_ctrl {
+	u16 bg_pal_bitfield;
+	u16 obj_pal_bitfield;
+	u16 scale;
+	u16 mix_color;
+	u8 field_8;
+	u8 field_9;
+	u8 field_A;
+	u8 field_B;	
+};
+
+struct fade_ctrl pal_fade_control;
+
 bool fade_screen(u32 bitmask, s8 unknown, u8 to, u8 from, u16 color);
 
 #endif /* ENGINE_VIDEO_H */
