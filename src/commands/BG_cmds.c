691cdd7dbe402434165cb2fdfe56ec2e26e6e754
EternalCode <dilshan.gunasekera@mail.utoronto.ca>
2015-11-04 09:36:59 -0500

    template files
    Created empty command files for commands related to of OAM and BG
    manipulation. Also fixed some mistakes and made slight adjustments to
    commands.yml

---
+++ src/commands/BG_cmds.c
@@ -0,0 +1,26 @@
+#include "../interpreter.h"
+#include "../engine/types.h"
+
+bool load_BG(arg_1, arg_2, arg_3, arg_4){
+	/* load a BG */
+}
+
+bool BG_display(arg_1){
+	/* display a BG */
+}
+
+bool BG_move(arg_1, arg_2, arg_3){
+	/* set a BG's coords by ID */
+}
+
+bool BG_del(arg_1){
+	/* delete a BG by ID*/
+}
+
+bool BG_translate(arg_1, arg_2, arg_3, arg_4){
+	/* translate a BG by ID to location at speed */
+}
+
+bool BG_pal_update(arg_1, arg_2){
+	/* update a BG's pal */
+}
