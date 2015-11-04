c67ad2d4bd519c581630798ec49dfd31ed0c4c42
Touched <jtouched@gmail.com>
2015-11-04 14:18:31 +0200

    Add script environment

---
+++ src/engine/script.h
@@ -0,0 +1,27 @@
+#ifndef ENGINE_SCRIPT_H
+#define ENGINE_SCRIPT_H
+
+#include "types.h"
+
+struct script_env {
+	u8 depth;
+	u8 mode;
+	u8 cmp_result;
+	u8 field_3;
+	u32 pointer_asm;
+	u32 pointer_script;
+	u32 stack[20];
+	u32 cmd_table;
+	u32 cmd_table_max;
+	u32 vars[4];
+	u8 enabled;
+	u8 field_75;
+	u8 keypad_control_enabled;
+	u8 keypad_override_direction;
+};
+
+extern struct script_env script_env_1;
+extern struct script_env script_env_2;
+
+#endif /* ENGINE_SCRIPT_H */
+	
