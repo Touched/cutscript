360aa1bbe771b05415432e5cf89f335744cd4d6c
EternalCode <dilshan.gunasekera@mail.utoronto.ca>
2015-11-04 09:36:59 -0500

    template files
    Created empty command files for commands related to of OAM and BG
    manipulation. Also fixed some mistakes and made slight adjustments to
    commands.yml

---
+++ src/commands/OAM_cmds.c
@@ -0,0 +1,38 @@
+#include "../interpreter.h"
+#include "../engine/types.h"
+
+bool oam_create(arg_1, arg_2, arg_3, arg_4, arg_5, arg_6, arg_7){
+	/* create a struct using args */
+}
+
+bool oam_display(???){
+	/* display OAM */
+}
+
+bool oam_move(arg_1, arg_2, arg_3){
+	/*set new x/y position*/
+}
+
+bool oam_del(arg_1){
+	/* delete oam */
+}
+
+bool oam_translate(arg_1){
+	/* translte an OAM to a position*/
+}
+
+bool oam_pal_update(arg_1, arg_2){
+	/* change pal of an OAM */
+}
+
+bool oam_rotate(arg_1, arg_2){
+	/* rotate OAM a direction */
+}
+
+bool oam_flip(arg_1, arg_2){
+	/* flip an oam a certain axis */
+}
+
+bool oam_zoom(arg_1, arg_2, arg_3){
+	/* Zoom into part of an OAM */
+}
