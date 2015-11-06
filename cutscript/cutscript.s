8c5a2efbce5201d7b2c4798f80bf8fbf23c7ae1d
Touched <jtouched@gmail.com>
2015-11-06 14:02:51 +0200

    Update command defintions

--- cutscript/cutscript.s
+++ cutscript/cutscript.s
@@ -19,52 +19,52 @@
 
 @ (u16 var_ID, &u32 local_var.index) -> [local_var.index] --> true
 @ sets the value of a local_var.index to be the value of a variable
-.macro cs_ow_var_load arg0, arg1
+.macro cs_ow_var_load arg1, arg0
 .byte 3
-.hword \arg0
 .word \arg1
+.hword \arg0
 .endm
 
 @ (u16 var_ID, u16 value) -> void --> true
 @ sets variable to the value of local_var.index. Note, only last two bytes are transfered.
-.macro cs_ow_var_set arg0, arg1
+.macro cs_ow_var_set arg1, arg0
 .byte 4
-.hword \arg0
 .hword \arg1
+.hword \arg0
 .endm
 
 @ (u16 var_ID, &u32 local_var.index) -> [local_var.index] --> true
 @ sets local_var.index to be equal to the pointer to variable.
-.macro cs_ow_var_access arg0, arg1
+.macro cs_ow_var_access arg1, arg0
 .byte 5
-.hword \arg0
 .hword \arg1
+.hword \arg0
 .endm
 
 @ (&u32 image, &u32 pal, &u32 local_var.index) -> [local_var.index] --> true
 @ creates an OAM given pointers to pal and image. Put's it's ID in local var index
-.macro cs_create_oam arg0, arg1, arg2
+.macro cs_create_oam arg1, arg2, arg0
 .byte 6
-.word \arg0
 .word \arg1
 .word \arg2
+.word \arg0
 .endm
 
 @ (&u32 local_var.index, u8 mode) -> void --> true
 @ Controls the display of a OAM with an ID stored in local_var.index.  Mode 0 = hide, Mode 1 = show
-.macro cs_oam_display arg0, arg1
+.macro cs_oam_display arg1, arg0
 .byte 7
-.word \arg0
 .byte \arg1
+.word \arg0
 .endm
 
 @ (&u32 local_var.index, u8 x, u8 y) -> void --> true
 @ Change X, Y position of an OAM with id in local_var.index.
-.macro cs_oam_move arg0, arg1, arg2
+.macro cs_oam_move arg1, arg2, arg0
 .byte 8
-.word \arg0
 .byte \arg1
 .byte \arg2
+.word \arg0
 .endm
 
 @ (&u32 local_var.index) -> [local_var.index] --> true
@@ -76,47 +76,47 @@
 
 @ (&u32 local_var.index, u8 speed, u8 x, u8 y) -> void --> true
 @ progressively translate an OAM with ID local_var.index at a speed to location X, Y by incrementing or decrementing
-.macro cs_oam_translate arg3, arg0, arg1, arg2
+.macro cs_oam_translate arg1, arg2, arg3, arg0
 .byte 10
-.byte \arg3
-.word \arg0
 .byte \arg1
 .byte \arg2
+.byte \arg3
+.word \arg0
 .endm
 
 @ (&u32 local_var.index, &u32 pal) -> void --> true
 @ update OAM pal to pal. Old pal is overwritten.
-.macro cs_oam_pal_update arg0, arg1
+.macro cs_oam_pal_update arg1, arg0
 .byte 11
-.word \arg0
 .word \arg1
+.word \arg0
 .endm
 
 @ (&u32 image, &u32 pal, &u32 tilemap, u8 slot) -> void --> true
 @ creates a BG in slot using resources provided
-.macro cs_BG_load arg3, arg0, arg1, arg2
+.macro cs_BG_load arg1, arg2, arg3, arg0
 .byte 12
-.byte \arg3
-.word \arg0
 .word \arg1
 .word \arg2
+.byte \arg3
+.word \arg0
 .endm
 
 @ (u8 ID, u8 mode) -> void --> true
 @ Controls the display of a OAM with an ID stored in local_var.index.  Mode 0 = hide, Mode 1 = show
-.macro cs_BG_display arg0, arg1
+.macro cs_BG_display arg1, arg0
 .byte 13
-.byte \arg0
 .byte \arg1
+.byte \arg0
 .endm
 
 @ (u8 ID, u8 x, u8 y) -> void --> true
 @ Change X, Y position of an BG.
-.macro cs_BG_move arg0, arg1, arg2
+.macro cs_BG_move arg1, arg2, arg0
 .byte 14
-.byte \arg0
 .byte \arg1
 .byte \arg2
+.byte \arg0
 .endm
 
 @ (u8 ID) -> void --> true
@@ -128,43 +128,43 @@
 
 @ (u8 ID, u8 speed, u16 x, u16 y) -> void --> true
 @ progressively translate an BG at a speed to location X, Y by incrementing or decrementing
-.macro cs_BG_translate arg3, arg0, arg1, arg2
+.macro cs_BG_translate arg1, arg2, arg3, arg0
 .byte 16
-.hword \arg3
-.byte \arg0
 .byte \arg1
 .hword \arg2
+.hword \arg3
+.byte \arg0
 .endm
 
 @ (&u32 local_var.index, &u32 pal) -> void --> true
 @ update BG pal to pal. Old pal is overwritten.
-.macro cs_BG_pal_update arg0, arg1
+.macro cs_BG_pal_update arg1, arg0
 .byte 17
-.word \arg0
 .word \arg1
+.word \arg0
 .endm
 
 @ (&u32 local_var.index, u8 direction) -> void --> true rotate an oam in a direction
-.macro cs_oam_rotate arg0, arg1
+.macro cs_oam_rotate arg1, arg0
 .byte 18
-.word \arg0
 .byte \arg1
+.word \arg0
 .endm
 
 @ (&u32 local_var.index, u8 mode) -> void --> true
 @ flip an oam on an axis
-.macro cs_oam_flip arg0, arg1
+.macro cs_oam_flip arg1, arg0
 .byte 19
-.word \arg0
 .byte \arg1
+.word \arg0
 .endm
 
 @ (&u32 local_var.index, u8 scale_size, u8 scale_mode) -> void --> true
 @ change the scaling on an OAM
-.macro cs_oam_zoom arg0, arg1, arg2
+.macro cs_oam_zoom arg1, arg2, arg0
 .byte 20
-.word \arg0
 .byte \arg1
 .byte \arg2
+.word \arg0
 .endm
 
