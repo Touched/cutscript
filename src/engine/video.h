95149add248b44e71d24eba269582b6e8301838f
Touched <jtouched@gmail.com>
2015-11-06 11:44:30 +0200

    Add vram and tilemap setup functions

--- src/engine/video.h
+++ src/engine/video.h
@@ -14,6 +14,19 @@ struct fade_ctrl {
 	u8 field_B;	
 };
 
+struct bg_config {
+	u8 active : 1;
+	u8 padding : 1;
+	u8 screen_size : 2;
+	u8 priority : 2;
+	u8 mosaic : 1;
+	u8 wraparound : 1;
+	u8 character_base : 2;
+	u8 screen_base : 5;
+	u8 palette : 1; 	/* 1: 256/1, 0: 16/16 */
+	u16 unknown;
+};
+
 struct fade_ctrl pal_fade_control;
 
 bool fade_screen(u32 bitmask, s8 speed, u8 to, u8 from, u16 color);
@@ -32,5 +45,7 @@ u16 gpu_copy_to_tileset(u8 layer, u8 *tiles, u16 size, u16 offset);
 void gpu_copy_tilemap(u8 layer, u8 *map, u16 size_or_null, u16 offset);
 void gpu_pal_apply(u8 *palette, u16 offset, u16 size);
 void bgid_send_tilemap(u8 layer);
+void bgid_set_tilemap(u8 layer, u8 *space);
+void bg_vram_setup(u8 layer, struct bg_config *config, u8 layers);
 
 #endif /* ENGINE_VIDEO_H */
