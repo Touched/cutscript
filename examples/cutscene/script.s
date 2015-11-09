096597f8c633993ac446db2fbce3c27976a4476c
Touched <jtouched@gmail.com>
2015-11-09 15:21:52 +0200

    Fix incorrect palette pointer

--- examples/cutscene/script.s
+++ examples/cutscene/script.s
@@ -3,7 +3,7 @@
 main:
 	cs_bg_load 1, 0x084147C0, 0x084154E8
 	cs_BG_display 1, 1
-	cs_pal_load 0x08515954, 0, 0x40
+	cs_pal_load 0x08415954, 0, 0x40
 	cs_pal_fade 0x3, 0x10, 0, 0x0000
 	cs_pause 0xFF
 	cs_end
